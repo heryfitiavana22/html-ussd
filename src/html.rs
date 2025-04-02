9f3c0ff234f76a294bea9c72a1eae1cf0bbbf845
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-04-02 11:49:47 +0300

    feat: introduce HtmlUssdTree and related structures for enhanced HTML representation

--- src/html.rs
+++ src/html.rs
@@ -20,6 +20,73 @@ pub struct TagElement {
     pub children: Vec<TagElement>,
 }
 
+#[derive(Debug, PartialEq, Clone)]
+pub struct HtmlUssdTree {
+    pub source: Html,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Html {
+    pub attributes: Vec<(String, String)>,
+    pub head: Head,
+    pub body: Body,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Head {
+    pub attributes: Vec<(String, String)>,
+    pub title: Title,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Title {
+    pub attributes: Vec<(String, String)>,
+    pub text: String,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Body {
+    pub attributes: Vec<(String, String)>,
+    pub paragraphs: Vec<Paragraph>,
+    pub content: BodyContent,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum BodyContent {
+    Form(Form),
+    Links(Vec<Link>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Form {
+    pub attributes: Vec<(String, String)>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Paragraph {
+    pub attributes: Vec<(String, String)>,
+    pub text: String,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Link {
+    pub attributes: Vec<(String, String)>,
+    pub text: String,
+    pub href: Href,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Href {
+    pub url: String,
+    pub href_type: HrefType
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum HrefType {
+    File,
+    Server,
+}
+
 impl Display for Tag {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
