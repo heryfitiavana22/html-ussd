5a23475025444ae552212bb0b59b274bd3d4981b
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-04-02 11:21:22 +0300

    refactor: enhance page validation logic and update error handling for HTML structure

--- src/page_validator.rs
+++ src/page_validator.rs
@@ -3,90 +3,109 @@ use crate::html::{Tag, TagElement};
 pub struct PageValidator;
 
 impl PageValidator {
-    pub fn validate(tag_elements: &[TagElement]) -> Result<(), PageValidatorError> {
-        let mut title_found = false;
+    pub fn validate(tag_elements: Vec<TagElement>) -> Result<(), PageValidatorError> {
         let mut form_found = false;
         let mut link_found = false;
 
-        for tag in tag_elements {
-            match &tag.tag_name {
-                &Tag::Head => {
-                    let title_count = tag
-                        .children
-                        .iter()
-                        .filter(|t| matches!(t.tag_name, Tag::Title))
-                        .count();
-                    if title_count != 1 {
-                        return Err(PageValidatorError::TitleMissingOrMultiple);
+        let option_html: Option<&TagElement> = tag_elements.get(0);
+        if option_html.is_none() {
+            return Err(PageValidatorError::TagNotFound(Tag::Html));
+        };
+        let html = option_html.unwrap();
+        if html.tag_name != Tag::Html {
+            return Err(PageValidatorError::TagNotFound(Tag::Html));
+        }
+        if html.children.len() > 2 {
+            return Err(PageValidatorError::UnexpectedChilds(html.children.clone()));
+        }
+
+        // <head>
+        let option_head = html.children.get(0);
+        if option_head.is_none() {
+            return Err(PageValidatorError::TagNotFound(Tag::Head));
+        };
+        let head = option_head.unwrap();
+        if head.tag_name != Tag::Head {
+            return Err(PageValidatorError::TagNotFound(Tag::Head));
+        }
+        if head.children.len() > 1 {
+            return Err(PageValidatorError::UnexpectedChilds(head.children.clone()));
+        }
+        let option_title = head.children.get(0);
+        if option_title.is_none() {
+            return Err(PageValidatorError::TagNotFound(Tag::Title));
+        };
+        let title = option_title.unwrap();
+        if title.tag_name != Tag::Title {
+            return Err(PageValidatorError::TagNotFound(Tag::Title));
+        }
+
+        // <body>
+        let option_body = html.children.get(1);
+        if option_body.is_none() {
+            return Err(PageValidatorError::TagNotFound(Tag::Body));
+        };
+        let body = option_body.unwrap();
+        if body.tag_name != Tag::Body {
+            return Err(PageValidatorError::TagNotFound(Tag::Body));
+        }
+        if body.children.len() == 0 {
+            return Err(PageValidatorError::EmptyBody);
+        }
+
+        for child_body in &body.children {
+            match &child_body.tag_name {
+                Tag::Text(_) | Tag::P => {}
+                Tag::Form => {
+                    if form_found {
+                        return Err(PageValidatorError::MutlipleForm);
                     }
-                    title_found = true;
-                }
-                Tag::Body => {
-                    for child in &tag.children {
-                        match &child.tag_name {
-                            Tag::Text(_) | Tag::P | Tag::Link | Tag::Form => {}
-                            _ => {
-                                return Err(PageValidatorError::UnexpectedTag(child.tag_name.clone()));
-                            }
+                    if link_found {
+                        return Err(PageValidatorError::FormAndLinkTogether);
+                    }
+                    form_found = true;
+
+                    if child_body.children.len() > 1 {
+                        return Err(PageValidatorError::UnexpectedChilds(
+                            child_body.children.clone(),
+                        ));
+                    }
+
+                    let option_input = child_body.children.get(0);
+                    if option_input.is_none() {
+                        return Err(PageValidatorError::FormMustHaveOneInput);
+                    };
+                    let input = option_input.unwrap();
+                    if input.tag_name != Tag::Input {
+                        return Err(PageValidatorError::UnexpectedTag(input.tag_name.clone()));
+                    }
+
+                    // <input type="..." />
+                    let type_attr = input.attributes.iter().find(|(k, _)| k == "type");
+                    if let Some((_, value)) = type_attr {
+                        if value != "text" && value != "number" {
+                            return Err(PageValidatorError::InvalidInputType(value.clone()));
                         }
+                    } else {
+                        return Err(PageValidatorError::MissingInputType);
+                    }
 
-                        // <a> ou un <form>, mais pas les deux
-                        match &child.tag_name {
-                            Tag::Form => {
-                                if form_found || link_found {
-                                    return Err(PageValidatorError::FormAndLinkTogether);
-                                }
-                                form_found = true;
-
-                                // Vérifier que le form a un seul input
-                                let input_count = child
-                                    .children
-                                    .iter()
-                                    .filter(|t| matches!(t.tag_name, Tag::Input))
-                                    .count();
-                                if input_count != 1 {
-                                    return Err(PageValidatorError::FormMustHaveOneInput);
-                                }
-
-                                // Vérifier le type de l'input
-                                if let Some(input) = child
-                                    .children
-                                    .iter()
-                                    .find(|t| matches!(t.tag_name, Tag::Input))
-                                {
-                                    let type_attr =
-                                        input.attributes.iter().find(|(k, _)| k == "type");
-                                    if let Some((_, value)) = type_attr {
-                                        if value != "text" && value != "number" {
-                                            return Err(PageValidatorError::InvalidInputType(
-                                                value.clone(),
-                                            ));
-                                        }
-                                    } else {
-                                        return Err(PageValidatorError::MissingInputType);
-                                    }
-                                }
-                            }
-                            Tag::Link => {
-                                if form_found {
-                                    return Err(PageValidatorError::FormAndLinkTogether);
-                                }
-                                link_found = true;
-                            }
-                            _ => {}
-                        }
+                    if input.children.len() > 0 {
+                        return Err(PageValidatorError::UnexpectedChilds(input.children.clone()));
                     }
                 }
-                _ => {}
+                Tag::Link => {
+                    if form_found {
+                        return Err(PageValidatorError::FormAndLinkTogether);
+                    }
+                    link_found = true;
+                }
+                _ => {
+                    return Err(PageValidatorError::UnexpectedTag(
+                        child_body.tag_name.clone(),
+                    ));
+                }
             }
-        }
-
-        if !title_found {
-            return Err(PageValidatorError::TitleMissingOrMultiple);
-        }
-
-        if !form_found && !link_found {
-            return Err(PageValidatorError::NoLinksOrForm);
         }
 
         Ok(())
@@ -94,11 +113,13 @@ impl PageValidator {
 }
 
 pub enum PageValidatorError {
-    TitleMissingOrMultiple,
+    TagNotFound(Tag),
     FormAndLinkTogether,
     UnexpectedTag(Tag),
+    UnexpectedChilds(Vec<TagElement>),
     FormMustHaveOneInput,
     InvalidInputType(String),
+    EmptyBody,
     MissingInputType,
-    NoLinksOrForm,
+    MutlipleForm,
 }
