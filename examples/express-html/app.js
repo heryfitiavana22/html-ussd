11c4cc0db904d0dfb127c160c76bd997be93d689
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-04-03 21:48:23 +0300

    feat: add express server with HTML pages for balance and payment functionalities

---
+++ examples/express-html/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+app.use(express.json());
+
+function renderFile(req, res, file) {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Erreur serveur");
+    } else {
+      res.set("Content-Type", "text/html");
+      res.send(data);
+    }
+  });
+}
+
+app.get("/", (req, res) => {
+  res.send("it's work")
+});
+
+app.get("/main-page", (req, res) => {
+  console.log("main-page");
+  renderFile(req, res, "./pages/main-page.html");
+});
+
+app.get("/paiment", (req, res) => {
+  console.log("paiment");
+
+  renderFile(req, res, "./pages/paiement.html");
+});
+
+app.get("/solde", (req, res) => {
+  console.log("solde");
+
+  renderFile(req, res, "./pages/solde.html");
+});
+
+app.get("/validate-code", (req, res) => {
+    console.log("valid-code");
+  
+    renderFile(req, res, "./pages/valid-code.html");
+  });
+
+app.use((req, res) => {
+  res.status(404).send("Page non trouvée");
+});
+
+app.listen(8888, () => console.log("Serveur en écoute sur le port 8888"));
